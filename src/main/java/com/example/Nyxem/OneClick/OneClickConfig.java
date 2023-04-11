d46864d7835515ef6e76e5dbd88c25d9c3ba305c
Nyxem64 <manamanor@hotmail.co.uk>
2023-04-11 17:57:57 +0100

    Added OneClick toggle Added config section Added more logic for potential one click

--- src/main/java/com/example/Nyxem/OneClick/OneClickConfig.java
+++ src/main/java/com/example/Nyxem/OneClick/OneClickConfig.java
@@ -3,17 +3,33 @@ package com.example.Nyxem.OneClick;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
-@ConfigGroup("custom")
+@ConfigGroup("oneclick")
 public interface OneClickConfig extends Config
 {
-	@ConfigItem(
-		keyName = "customItem",
-		name = "Custom Item Config",
-		description = "Enter item ids. Format id:id"
+	@ConfigSection(
+		position = 0,
+		name = "Custom Items Config",
+		description = ""
 	)
-	default String getCustomItemIDs()
-	{
-		return "0:0";
-	}
+	String customItemsConfig = "Custom Items Config";
+
+	@ConfigItem(
+			position = 0,
+			keyName = "customItemToggle",
+			name = "Custom Item Toggle",
+			description = "Turn Custom Item One Clicks on or off",
+			section = customItemsConfig
+	)
+	default boolean isCustomItemOneClickEnabled() { return true; }
+
+	@ConfigItem(
+		position = 1,
+		keyName = "customItemIDs",
+		name = "Custom Item IDs",
+		description = "Enter item ids. Format id:id. When you hover over any of two IDs, it will change to use on the other id. Toggle must be on.",
+		section = customItemsConfig
+	)
+	default String getCustomItemIDs() { return "0:0"; }
 }
--- src/main/java/com/example/Nyxem/OneClick/OneClickPlugin.java
+++ src/main/java/com/example/Nyxem/OneClick/OneClickPlugin.java
@@ -1,22 +1,26 @@
 package com.example.Nyxem.OneClick;
 
 import com.example.EthanApiPlugin.Inventory;
+import com.example.InteractionApi.InventoryInteraction;
 import com.example.Packets.MousePackets;
 import com.example.Packets.WidgetPackets;
 import com.google.inject.Provides;
 import com.google.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
+import net.runelite.api.*;
 import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
-import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import org.apache.commons.lang3.StringUtils;
+
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Slf4j
@@ -35,34 +39,32 @@ public class OneClickPlugin extends Plugin
 	@Inject
 	private OneClickConfig config;
 
-	int timeout = 0;
-
 	@Override
 	protected void startUp() throws Exception {
-		log.info("message");
+		log.info("Nyxem Plugin Start");
 	}
 
 	@Override
 	protected void shutDown() throws Exception {
-		log.info("Example stopped!");
+		log.info("Nyxem Plugin stopped!");
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged) {
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
-			//client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			log.info("logged in");
 		}
 	}
 
 	@Subscribe
 	public void onGameTick(GameTick tiktok) {
 
-		log.info("tick");
-		handleAction(config.getCustomItemIDs());
-
+		if (config.isCustomItemOneClickEnabled()) {
+			handleItemAction(config.getCustomItemIDs());
+		}
 	}
 
-	public void handleAction(String actionParam) {
+	private void handleItemAction(String actionParam) {
 
 		String[] Actions = actionParam.split("\n");
 		for (String Action : Actions) {
@@ -72,28 +74,51 @@ public class OneClickPlugin extends Plugin
 			String firstItemParse = Action.split(":")[0];
 			String secondItemParse = Action.split(":")[1];
 
+			int firstItemID = Integer.parseInt(firstItemParse);
+			int secondItemID = Integer.parseInt(secondItemParse); // thought I needed these for something
+
+
 			Optional<Widget> firstItem = StringUtils.isNumeric(firstItemParse) ?
-					Inventory.search().withId(Integer.parseInt(firstItemParse)).first() :
+					Inventory.search().withId(firstItemID).first() :
 					Inventory.search().matchesWildCardNoCase(firstItemParse).first();
 
 			Optional<Widget> secondItem = StringUtils.isNumeric(secondItemParse) ?
-					Inventory.search().withId(Integer.parseInt(secondItemParse)).first() :
+					Inventory.search().withId(secondItemID).first() :
 					Inventory.search().matchesWildCardNoCase(secondItemParse).first();
 
-			if (firstItem.isEmpty()) {
-				break;
+			if(firstItem.isEmpty()) {
+				break; // check item exists
 			}
 
 			if(secondItem.isEmpty()) {
-				break;
+				break; // check item exists
 			}
 
-			log.info("Attempting use");
-			MousePackets.queueClickPacket();
-			WidgetPackets.queueWidgetOnWidget(firstItem.get(), secondItem.get());
+			log.info("trying menus");
+			MenuEntry[] entries = this.client.getMenuEntries();
+			for (MenuEntry entry : entries) {
+				log.info("menu if 1"); // can probably be better
+				if (entry.getTarget().contains(firstItem.get().getName())) {
+					log.info("menu1");
+					client.createMenuEntry(1)
+							.setOption("One Click")
+							.setTarget(secondItem.get().getName());
+							// .onClick(ItemOnItem()); // future, remove comment & get widgets of use on item1 to item2
+				}
+				log.info("menu if 2");
+				if (entry.getTarget().contains(secondItem.get().getName())) {
+					log.info("menu2");
+					client.createMenuEntry(1).setOption("One Click").setTarget(firstItem.get().getName());
+				}
+			}
 		}
 	}
 
+	private void ItemOnItem(Widget widget1, Widget widget2)
+	{
+		MousePackets.queueClickPacket();
+		WidgetPackets.queueWidgetOnWidget(widget1, widget2);
+	}
 	@Provides
 	OneClickConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(OneClickConfig.class);
