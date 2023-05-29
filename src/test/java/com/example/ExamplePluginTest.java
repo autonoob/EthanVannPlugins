43208f85515e42927d1cef8fd8c6740f0c9d4cf0
autonoob <mikeylev@hotmail.co.uk>
2023-05-29 02:10:38 +0100

    Revert "project: support Kotlin / Add run configs (#1)"
    This reverts commit fffb0dde4f0284da86ec8e8e22fd94cc39dc6808.

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,26 +1,25 @@
 package com.example;
 
+import com.example.AutoTele.AutoTele;
+import com.example.E3t4g.e3t4g;
 import com.example.EthanApiPlugin.EthanApiPlugin;
-import com.example.PacketUtils.PacketUtilsPlugin;
+import com.example.LavaRunecrafter.LavaRunecrafterPlugin;
+import com.example.NightmareHelper.NightmareHelperPlugin;
 import com.example.PrayerFlicker.EthanPrayerFlickerPlugin;
-import com.example.ezclick.EZClickPlugin;
-import com.example.tooltips.TooltipsPlugin;
+import com.example.UpkeepPlugin.UpkeepPlugin;
+import com.example.gauntletFlicker.gauntletFlicker;
+import com.example.harpoon2ticker.SwordFish2Tick;
+import com.example.superglass.SuperGlassMakerPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(
-				//Ethan
-				EthanApiPlugin.class, PacketUtilsPlugin.class,
-
-				EthanPrayerFlickerPlugin.class,
-
-				//Null
-				TooltipsPlugin.class, EZClickPlugin.class);
-
-		RuneLite.main(args);
-	}
+public class ExamplePluginTest {
+    public static void main(String[] args) throws Exception {
+        ExternalPluginManager.loadBuiltin(EthanApiPlugin.class, PacketUtilsPlugin.class, EthanPrayerFlickerPlugin.class,
+                gauntletFlicker.class,
+                SuperGlassMakerPlugin.class, UpkeepPlugin.class, LavaRunecrafterPlugin.class,
+                NightmareHelperPlugin.class, SwordFish2Tick.class
+                , e3t4g.class, AutoTele.class);
+        RuneLite.main(args);
+    }
 }
